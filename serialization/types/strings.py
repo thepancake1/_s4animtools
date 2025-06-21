3a09064f88f4369fbaf96d8b6a1ba9302f7a88e7
thepancake1 <theojabalde@gmail.com>
2025-06-22 04:30:48 +0800

    Remove ASCIIString as it wasn't used anywhere. Move IOString to strings

--- serialization/types/strings.py
+++ serialization/types/strings.py
@@ -1,28 +1,15 @@
 from s4animtools.serialization.types.basic import UInt32, String
 from s4animtools.serialization import Serializable
 
-"""
-Transform Types are types used for representing exported data 
-that build on top on the Basic types but are used for things 
-like a Vector3 or a Quaternion.
-"""
-
-
-class ASCIIString(Serializable):
-    def __init__(self, length, string):
-        self.length = length
+class IOString(Serializable):
+    def __init__(self, string):
         self.string = string
 
-    def __iter__(self):
-        return iter([self.x, self.y, self.z])
-
     @staticmethod
-    def from_str(string):
-        if string.isascii():
-            binary_string = struct.encode("ascii")
-            return ASCIIString(len(binary_string), binary_string)
-        raise ValueError("A non-ascii string was passed into ascii string. Got: {}".format(string))
+    def from_binary(reader):
+        length = reader.u32()
+        value = reader.read_string(length)
+        return IOString(value)
 
     def to_binary(self):
-        return [UInt32(self.length), String(self.string)]
-
+        return UInt32(len(self.string)).serialize() + self.string.encode("ascii")
