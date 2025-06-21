68937372498494777e96a331b3682e4761e07220
thepancake1 <theojabalde@gmail.com>
2025-06-22 04:31:17 +0800

    Add from_binary function to SlotAssignment

--- slot_assignments.py
+++ slot_assignments.py
@@ -1,4 +1,5 @@
 from s4animtools.serialization.types.basic import UInt32, UInt16
+from s4animtools.serialization.types.strings import IOString
 
 
 class SlotAssignment:
@@ -8,6 +9,12 @@ class SlotAssignment:
         self._actor = actor
         self._target = target
 
+    @staticmethod
+    def from_binary(reader):
+        chain_idx = reader.u16()
+        slot_idx = reader.u16()
+        target_object_namespace = IOString.from_binary(reader)
+        target_joint_name = IOString.from_binary(reader)
 
     def serialize(self):
         return [UInt16(self._chainIdx).serialize(), UInt16(self._slotIdx).serialize(), UInt32(len(self._actor)).serialize(),
