912145eb7f3059a57b1092cf3db2d4412a7f6683
thepancake1 <theojabalde@gmail.com>
2025-06-22 04:10:27 +0800

    Remove unused f1 palette functions

--- clip_processing/f1_palette.py
+++ clip_processing/f1_palette.py
@@ -14,24 +14,6 @@ class F1Palette:
     def palette_size(self):
         return len(self.palette_values)
 
-    def get_palette(self, index):
-        """
-        Returns a palette from the F1 palette.
-        :param index: The index of the palette to return.
-        :return: A palette from the F1 palette.
-        """
-        return self.data[index * 4:index * 4 + 4]
-
-    def get_palette_index(self, palette_value):
-        """
-        Returns the index of a palette in the F1 palette.
-        :param palette_value: The palette value to find the index of.
-        """
-        for palette_value in self.palette_values:
-            if palette_value == palette_value:
-                return self.palette_values.index(palette_value)
-        return -1
-
     def try_add_palette_to_palette_values(self, potential_palette_value):
         """
         This checks if a potential palette value already exists in the list, if it already exists, it will be skipped.
