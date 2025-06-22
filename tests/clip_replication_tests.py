16adb995902bcff2415015fd89de07e651213ced
thepancake1 <theojabalde@gmail.com>
2025-06-22 15:59:54 +0800

    Add a test for importing rigs

---
+++ tests/clip_replication_tests.py
@@ -0,0 +1,26 @@
+import unittest
+import bpy
+
+from s4animtools.rig.create_rig import create_rig_with_context
+from s4animtools.bone_names import human_bones
+
+class TestS4AnimTools(unittest.TestCase):
+    expected_rig_name = "8EAF13DE!00000000!D057FCC534C1BCBB.Rig.binary"
+
+    def setUp(self):
+        from s4animtools import register
+        register()
+    def tearDown(self):
+        pass
+
+    def test_rig_import(self):
+        # Load the rig from the file
+        create_rig_with_context("test_data\\8EAF13DE!00000000!D057FCC534C1BCBB.Rig.binary", bpy.context)
+        # Check if the rig is loaded correctly
+        self.assertTrue(self.expected_rig_name in bpy.data.objects, "Rig not found in the file")
+
+        # Check if the rig has the expected bones
+        rig = bpy.data.objects[self.expected_rig_name]
+        expected_bones = human_bones
+        for bone in expected_bones:
+            self.assertIn(bone, rig.pose.bones, f"Bone {bone} not found in the rig")
---
+++ tests/test_template.py
@@ -0,0 +1,13 @@
+# A template for writing tests for the s4animtools package.
+
+import unittest
+
+class TestS4AnimTools(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_example_functionality(self):
+        self.assertTrue(True, "This is a placeholder test")
