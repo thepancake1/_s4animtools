6601c71561f60e7c3a98b9d8d8f3509adfbb24f5
thepancake1 <theojabalde@gmail.com>
2025-06-23 09:28:30 +0800

    Add NullTerminatedString

--- serialization/types/strings.py
+++ serialization/types/strings.py
@@ -13,3 +13,20 @@ class IOString(Serializable):
 
     def to_binary(self):
         return u32(len(self.string)).to_binary() + self.string.encode("ascii")
+
+class NullTerminatedString(Serializable):
+    def __init__(self, string):
+        self.string = string
+
+    @staticmethod
+    def from_binary(reader):
+        current_char = reader.read(1)
+        value = bytearray([current_char])
+        while current_char != b'\x00':
+            current_char = reader.read(1)
+            value.append(current_char)
+        # Convert bytearray to string, stripping the null terminator
+        return NullTerminatedString(value.decode("ascii").rstrip('\x00'))
+
+    def to_binary(self):
+        return self.string.encode("ascii") + b'\x00'
